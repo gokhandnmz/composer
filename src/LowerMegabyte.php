b85316fdba804caf0ed0fc05e0df01217ea7ccbe
gokhandnmz <gokhandnmz@outlook.com>
2022-12-21 00:54:12 +0300

    composer

---
+++ src/LowerMegabyte.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace LowerMegabyte;
+
+class LowerMegabyte
+{
+    public function getSize()
+    {
+        return "Image width: 100px and height:100px;";
+    }
+}
+
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'laravel/lowermegabyte',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'laravel/lowermegabyte' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
